7b3a539e0aef1542b6b09246973db7e628f1050b
ixiflutter1313 <mj.parishani@outlook.com>
2020-11-27 22:10:24 +0330

    Update ArticleContentGridListActivity.java

--- app/src/main/java/ntk/android/academy/activity/ArticleContentGridListActivity.java
+++ app/src/main/java/ntk/android/academy/activity/ArticleContentGridListActivity.java
@@ -1,40 +1,21 @@
 package ntk.android.academy.activity;
 
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import butterknife.BindView;
-import butterknife.OnClick;
-import es.dmoral.toasty.Toasty;
 import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.schedulers.Schedulers;
 import java9.util.function.Function;
 import ntk.android.academy.R;
 import ntk.android.academy.adapter.ArticleGridAdapter;
 import ntk.android.base.activity.abstraction.AbstractionListActivity;
-import ntk.android.base.config.NtkObserver;
 import ntk.android.base.entitymodel.article.ArticleContentModel;
 import ntk.android.base.entitymodel.base.ErrorException;
 import ntk.android.base.entitymodel.base.FilterDataModel;
 import ntk.android.base.services.article.ArticleContentService;
-import ntk.android.base.utill.EndlessRecyclerViewScrollListener;
 
 public class ArticleContentGridListActivity extends AbstractionListActivity<ArticleContentModel> {
-
-    @BindView(R.id.lblTitleActArticleContentList)
-    TextView Lbl;
-
-    @BindView(R.id.recyclerActArticleContentList)
-    RecyclerView Rv;
-
     @Override
     public void afterInit() {
         super.afterInit();
@@ -63,8 +44,5 @@ public class ArticleContentGridListActivity extends AbstractionListActivity<Arti
 
     }
 
-    @OnClick(R.id.imgBackArticleContentList)
-    public void Back() {
-        finish();
-    }
+
 }
