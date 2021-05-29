c29ca03b3df9fedb367101f634cf75748bfec018
ixiflutter1313 <mj.parishani@outlook.com>
2021-05-29 16:51:24 +0430

    Update MyApplication.java

--- app/src/main/java/ntk/android/academy/MyApplication.java
+++ app/src/main/java/ntk/android/academy/MyApplication.java
@@ -16,7 +16,6 @@ import ntk.android.base.ApplicationParameter;
 import ntk.android.base.ApplicationStaticParameter;
 import ntk.android.base.ApplicationStyle;
 import ntk.android.base.NTKApplication;
-import ntk.android.base.utill.FontManager;
 import ntk.android.base.view.ViewController;
 
 public class MyApplication extends NTKApplication {
@@ -25,8 +24,27 @@ public class MyApplication extends NTKApplication {
 
     @Override
     public void onCreate() {
-        DEBUG = true;
+        applicationStyle = new ApplicationStyle() {
+            @Override
+            public ViewController getViewController() {
+                ViewController vc = new ViewController() {
+                };
+                vc.setLoading_view(R.layout.app_base_loading)
+                        .setEmpty_view(R.layout.app_base_empty)
+                        .setError_view(R.layout.app_base_error)
+                        .setError_button(R.id.btn_error_tryAgain)
+                        .setError_label(R.id.tvError);
+                return vc;
+            }
+
+            @Override
+            public Class<?> getMainActivity() {
+                return MainActivity.class;
+            }
+
+        };
         super.onCreate();
+        DEBUG = true;
         if (!new File(getCacheDir(), "image").exists()) {
             new File(getCacheDir(), "image").mkdirs();
         }
@@ -43,21 +61,14 @@ public class MyApplication extends NTKApplication {
         Toasty.Config.getInstance()
                 .setToastTypeface(ntk.android.base.utill.FontManager.T1_Typeface(getApplicationContext()))
                 .setTextSize(14).apply();
-        applicationStyle = new ApplicationStyle() {
-            public ViewController getViewController() {
-                ViewController vc = new ViewController() {
-                };
-                vc.setLoading_view(R.layout.sub_base_loading);
-                vc.setEmpty_view(R.layout.sub_base_empty);
-                vc.setError_view(R.layout.sub_base_error);
-                return vc;
-            }
 
-            @Override
-            public Class<?> getMainActivity() {
-                return MainActivity.class;
-            }
-        };
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        instance = this;
+        super.attachBaseContext(base);
+        MultiDex.install(base);
     }
 
     @Override
@@ -65,12 +76,6 @@ public class MyApplication extends NTKApplication {
 //        ApplicationStaticParameter.URL = "http://01ab9643fde5.ngrok.io";
         ApplicationStaticParameter.PACKAGE_NAME = "ntk.android.academy.app72";
         return super.getConfig();
-    }
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(base);
     }
 
     @Override
